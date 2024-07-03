48f414d7aeda697c0bde16faa68d4dbdf8273f7c
Rahul Ganesh Sawant <rahulsawant8433@gmail.com>
2024-07-03 18:05:39 +0100

    Added Radio button@

--- dashboard/Soul.py
+++ dashboard/Soul.py
@@ -1,4 +1,4 @@
-from dash import Dash,html,dcc
+from dash import Dash, dcc, html, Input, Output, callback
 import plotly.express as plt
 import pandas as pd
 
@@ -6,13 +6,32 @@ df= pd.read_csv(r"C:\Users\rahul\OneDrive\Desktop\Forage\Quantium_\Jupyter\outpu
 
 app = Dash(__name__)
 
-fig = plt.line(df,x='date',y='sales',color='region')
+
 
 app.layout = html.Div(children=[
     html.H1(children='Sales of soul food over different dates'),
-
-    dcc.Graph(id='line-chart',figure=fig)
+    dcc.RadioItems(options=[
+       {'label': 'North', 'value': 'north'},
+        {'label': 'East', 'value': 'east'},
+        {'label': 'South', 'value': 'south'},
+        {'label': 'West', 'value': 'west'},
+        {'label': 'All', 'value': 'all'}
+    ],value='all',id='region',inline=True),
+    dcc.Graph(id='line-chart')
 ])
+
+@callback(
+    Output('line-chart','figure'),
+    Input('region','value')
+)
+def update_region(region_value):
+    if region_value == 'all':
+        filtered_df = df
+    else:
+        filtered_df = df[df['region']==region_value]
+
+    fig = plt.line(filtered_df,x='date',y='sales')
+    return fig
 
 if __name__ =='__main__':
     app.run(debug=True)
